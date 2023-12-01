ae000401b92303167cd28b57e61a9f367035317b
Igor Serebryany <igor47@moomers.org>
2023-11-30 18:32:57 -0800

    get htmx working
    /new loads as both static and dynamic based on header

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Header
 from fastapi.responses import HTMLResponse
+
+from typing import Annotated
 
 import templates
 
@@ -9,4 +11,10 @@ app = FastAPI()
 
 @app.get("/", response_class=HTMLResponse)
 async def index():
-    return templates.index()
+    return templates.index(as_frag=False)
+
+
+@app.get("/new", response_class=HTMLResponse)
+async def new(hx_request: Annotated[bool, Header()] = False):
+    print('hx_request', hx_request)
+    return templates.new(as_frag=hx_request)
--- templates.py
+++ templates.py
@@ -4,6 +4,18 @@ from jinja2 import Environment, FileSystemLoader
 env = Environment(loader=FileSystemLoader('fragments'))
 
 
-def index() -> str:
-    template = env.get_template('index.html')
-    return template.render()
+def base(as_frag: bool = False):
+    if as_frag:
+        return env.get_template('fragment.html')
+    else:
+        return env.get_template('page.html')
+
+
+def index(as_frag: bool = False) -> str:
+    template = env.get_template('index.html', )
+    return template.render(layout=base(as_frag))
+
+
+def new(as_frag: bool = False) -> str:
+    template = env.get_template('new.html')
+    return template.render(layout=base(as_frag))
